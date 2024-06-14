5b19421659c90f50cd0bea0d75ad9da416a10b34
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2024-06-14 15:24:48 +0300

    Done homework arrays

---
+++ 02_HomeworkArray/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _02_HomeworkArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
